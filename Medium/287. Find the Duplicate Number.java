b0f85d409a8509082edda868620e3d1a8b1b8071
Rishabh Kumar <70275515+crishabhkumar@users.noreply.github.com>
2021-09-29 13:44:12 +0530

    Update 287. Find the Duplicate Number.java

--- Medium/287. Find the Duplicate Number.java
+++ Medium/287. Find the Duplicate Number.java
@@ -1,4 +1,5 @@
 /*
+//Amazon question
 Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
 
 There is only one repeated number in nums, return this repeated number.
