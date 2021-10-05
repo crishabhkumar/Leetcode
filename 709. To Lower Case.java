6c37d70217ada225d8b3f61243cd08f48022b366
Rishabh Kumar <70275515+crishabhkumar@users.noreply.github.com>
2021-10-05 14:46:16 +0530

    Create 709. To Lower Case.java

---
+++ 709. To Lower Case.java
@@ -0,0 +1,30 @@
+/*
+Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+Example 1:
+
+Input: s = "Hello"
+Output: "hello"
+Example 2:
+
+Input: s = "here"
+Output: "here"
+Example 3:
+
+Input: s = "LOVELY"
+Output: "lovely"
+ 
+
+Constraints:
+
+1 <= s.length <= 100
+s consists of printable ASCII characters.
+*/
+
+
+
+//Solution:
+class Solution {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
