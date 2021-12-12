289023f17e4e743ae1124788e0900ffef5f321b4
Rishabh Kumar <crishabhkumar@gmail.com>
2021-12-12 19:03:00 +0530

    Solution 12-Dec-2021

---
+++ Medium/416. Partition Equal Subset Sum.java
@@ -0,0 +1,57 @@
+/*
+Given a non-empty array nums containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
+
+ 
+
+Example 1:
+
+Input: nums = [1,5,11,5]
+Output: true
+Explanation: The array can be partitioned as [1, 5, 5] and [11].
+Example 2:
+
+Input: nums = [1,2,3,5]
+Output: false
+Explanation: The array cannot be partitioned into equal sum subsets.
+ 
+
+Constraints:
+
+1 <= nums.length <= 200
+1 <= nums[i] <= 100
+*/
+
+
+
+//Solution:
+class Solution {
+    
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        int n = nums.length;
+        
+        for(int i : nums) sum+=i;
+        
+        if(sum%2!=0) return false;
+        
+        sum /= 2;
+        
+        boolean dp[][] = new boolean[n+1][sum+1];
+        
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=sum;j++){
+                if(i==0 || j==0)
+                    dp[i][j] = false;
+                else if(nums[i-1] > j)     // if curr sum value is greater than the current element value then just skip(take previous value)
+                    dp[i][j] = dp[i-1][j];
+                else if(nums[i-1]==j)  // we got required sum
+                    dp[i][j] = true;
+                else
+                    dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i-1]];
+            }
+        }
+        return dp[n][sum];
+        
+        
+    }
+}
